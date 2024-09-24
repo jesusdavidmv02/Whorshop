f4742656415525110a088d7dc274b76a75b87915
jesus mendoza <jesusdavidmv02@gmail.com>
2024-09-24 11:39:47 -0500

    Ejercicio nro 04

--- Persona.ts
+++ Persona.ts
@@ -1,12 +1,18 @@
 export class Persona {
     
     nombre: string = "";
-    edad: number  = 0;
+    private edad: number = 0;
+    
 
     constructor(nombre : string , edad : number)  {
         this.nombre = nombre
         this.edad= edad
     }
+    
+    getEdad(): number {
+        return this.edad;
+    }
+    
     
     saludar(): void{
         console.log(`Tu nombre es : ${this.nombre}  y tiene : ${this.edad} años`);
