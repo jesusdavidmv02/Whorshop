c8b69d50bf95dc011d0f19f718a298281a225878
jesus mendoza <jesusdavidmv02@gmail.com>
2024-09-26 11:30:04 -0500

    Ejercicio nro 13

--- jsonEmpleado.ts
+++ jsonEmpleado.ts
@@ -1,3 +1,24 @@
+import { Empleado } from "./Empleado"
+
+export const objEmpledo = (JSON : {nombre : string, edad : number,  direccion : {calle:string , ciudad :string, pais: string} , salario: number}) : Empleado  =>  {
+
+    const empleado = new Empleado(JSON.nombre, JSON.edad ,  JSON.direccion, JSON.salario);
+    return empleado 
+
+};
+
+function  debueveJson ( empleado : Empleado) : object {
+
+    const repueta = {
+        nombre : empleado.nombre,
+        edad : empleado.getEdad(),
+        direccion : empleado.direccion ,
+        salario : empleado.salario
+    }
+    return  repueta
+
+} 
+
 const jsonEmpleado = [ 
     { 
         nombre :  "Jesus David --- empleado json",
@@ -56,3 +77,24 @@ const jsonEmpleado = [
         salario :  10000
     } 
 ];
+
+
+// console.log(jsonEmpleado);
+// jsonEmpleado.forEach((jsonEmpleado) => objEmpledo(jsonEmpleado))
+
+const emp  : Empleado [] = [];
+
+console.log(" \n ___________ En la Clase empleados : __________  \n ");
+
+for (let i = 0; i < jsonEmpleado.length; i++) {
+   const aux  = objEmpledo(jsonEmpleado[i]);
+   emp.push(aux);
+   console.log(aux);
+}
+
+console.log(" \n  ______________En json : _______________ \n ");
+
+for (let i = 0; i < emp.length; i++) {
+    const aux  = debueveJson(emp[i]);
+    console.log(aux);
+ }
